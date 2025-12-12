5db6136dead501aba54a82e159334b7630a6b964
bolsterstone <jerukim419@gmail.com>
2025-12-13 06:12:17 +0900

    sad

--- CacheMemory-Gun-Smith-of-Dungeon/Assets/02.KYR/KYR_01.Scripts/Generation/corrider/CorridorStretch.cs
+++ CacheMemory-Gun-Smith-of-Dungeon/Assets/02.KYR/KYR_01.Scripts/Generation/corrider/CorridorStretch.cs
@@ -3,29 +3,52 @@ using UnityEngine;
 
 public class CorridorStretch : MonoBehaviour
 {
-    [SerializeField] private bool isHorizontal = true; //세로 false        
-    [SerializeField] private List<SpriteRenderer> renderers;   
+    [SerializeField] private bool isHorizontal = true;              // 세로 복도면 false
+    [SerializeField] private List<SpriteRenderer> renderers;        
+    [SerializeField] private List<BoxCollider2D> colliders;        
 
     public void SetBetween(Vector3 doorA, Vector3 doorB)
     {
         Vector3 mid = (doorA + doorB) * 0.5f;
         transform.position = mid;
-        
+
         float length = isHorizontal
             ? Mathf.Abs(doorB.x - doorA.x)
             : Mathf.Abs(doorB.y - doorA.y);
-        
-        foreach (var sr in renderers)
+
+        for (int i = 0; i < renderers.Count; i++)
         {
+            var sr = renderers[i];
             if (sr == null) continue;
 
-            Vector2 size = sr.size;
+            var s = sr.size;
             if (isHorizontal)
-                size.x = length;
+                s.x = length;
             else
-                size.y = length;
+                s.y = length;
+            sr.size = s;
 
-            sr.size = size;
+            if (i < colliders.Count && colliders[i] != null)
+            {
+                var col = colliders[i];
+
+                var colSize   = col.size;
+                var colOffset = col.offset;
+
+                if (isHorizontal)
+                {
+                    colSize.x   = length;
+                    colOffset.x = 0f;     
+                }
+                else
+                {
+                    colSize.y   = length;
+                    colOffset.y = 0f;
+                }
+
+                col.size   = colSize;
+                col.offset = colOffset;
+            }
         }
     }
 }
