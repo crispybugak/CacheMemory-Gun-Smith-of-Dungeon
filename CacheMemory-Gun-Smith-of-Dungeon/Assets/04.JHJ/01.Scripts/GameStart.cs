0ed8a33b27bb53b7da17c07954a654563400ffde
hyeonjin0209 <jonmoshyeonmin@gmail.com>
2025-12-13 23:08:01 +0900

    fix : game start scripts

--- CacheMemory-Gun-Smith-of-Dungeon/Assets/04.JHJ/01.Scripts/GameStart.cs
+++ CacheMemory-Gun-Smith-of-Dungeon/Assets/04.JHJ/01.Scripts/GameStart.cs
@@ -24,7 +24,6 @@ public class GameStart : MonoBehaviour
     private void Start()
     {
         Debug.Log("ddd");
-        AudioManager.Instance.PlaySound("타이틀", 0.6f, 1);
         Sequence sequence = DOTween.Sequence();
         if(gameLogoTop != null && gameLogoBottom != null)
         {
