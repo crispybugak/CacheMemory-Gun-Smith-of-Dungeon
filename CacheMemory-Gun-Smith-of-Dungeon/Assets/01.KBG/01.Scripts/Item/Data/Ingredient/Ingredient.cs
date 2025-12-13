fcbcf9c94c944c4201966553e6499a4e6c9451fb
soso7194 <wons5215@naver.com>
2025-12-13 15:34:22 +0900

    feat creft

--- CacheMemory-Gun-Smith-of-Dungeon/Assets/01.KBG/01.Scripts/Item/Data/Ingredient/Ingredient.cs
+++ CacheMemory-Gun-Smith-of-Dungeon/Assets/01.KBG/01.Scripts/Item/Data/Ingredient/Ingredient.cs
@@ -14,11 +14,19 @@ namespace KBG.Item
     
     
     [CreateAssetMenu(menuName = "SO/Item/Ingredient")]
-    public class Ingredient : ScriptableObject, IItemData
+    public class Ingredient : IItem, IItemData
     {
         [field:SerializeField] public Sprite icon { get; set; }
         [field:SerializeField] public float upScaling { get; set; }
         [field:SerializeField] public string itemName { get; set; }
         public IngredientType type;
+
+        // 변경점 2: IItem 추상 클래스의 필수 속성을 구현합니다.
+        // Ingredient는 그 자체로 데이터이므로 자기 자신(this)을 반환합니다.
+        public override IItemData ItemData 
+        { 
+            get => this; 
+            set { /* 데이터 자체가 자신이므로 set은 비워둡니다 */ } 
+        }
     }
 }
