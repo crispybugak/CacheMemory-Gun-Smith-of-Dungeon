95f68bc6af00966a1f728643b9fcca66902d96e4
crispybugak <rhylawd@gmail.com>
2025-12-13 23:54:36 +0900

    feat : reduce ingredient

--- CacheMemory-Gun-Smith-of-Dungeon/Assets/01.KBG/01.Scripts/Item/Data/Ingredient/Ingredient.cs
+++ CacheMemory-Gun-Smith-of-Dungeon/Assets/01.KBG/01.Scripts/Item/Data/Ingredient/Ingredient.cs
@@ -3,30 +3,23 @@ using UnityEngine;
 
 namespace KBG.Item
 {
-    [Flags]
     public enum IngredientType
     {
-        None = 0,
-        Polyamide = 1 << 0,
-        Steel = 1 << 1,
-        StainlessSteel = 1 << 2
+        None,
+        Polyamide,
+        Steel,
+        StainlessSteel,
+        GunPowder
     }
     
     
     [CreateAssetMenu(menuName = "SO/Item/Ingredient")]
-    public class Ingredient : IItem, IItemData
+    public class Ingredient : ScriptableObject, IItemData
     {
         [field:SerializeField] public Sprite icon { get; set; }
         [field:SerializeField] public float upScaling { get; set; }
         [field:SerializeField] public string itemName { get; set; }
         public IngredientType type;
-
-        // 변경점 2: IItem 추상 클래스의 필수 속성을 구현합니다.
-        // Ingredient는 그 자체로 데이터이므로 자기 자신(this)을 반환합니다.
-        public override IItemData ItemData 
-        { 
-            get => this; 
-            set { /* 데이터 자체가 자신이므로 set은 비워둡니다 */ } 
-        }
+        
     }
 }
