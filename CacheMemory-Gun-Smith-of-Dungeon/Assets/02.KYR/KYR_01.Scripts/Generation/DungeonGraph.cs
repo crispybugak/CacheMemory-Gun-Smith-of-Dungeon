33228437ebe8c6927c3899ef66d45f4a77b685af
bolsterstone <jerukim419@gmail.com>
2025-12-14 14:06:39 +0900

    wintermap

--- CacheMemory-Gun-Smith-of-Dungeon/Assets/02.KYR/KYR_01.Scripts/Generation/DungeonGraph.cs
+++ CacheMemory-Gun-Smith-of-Dungeon/Assets/02.KYR/KYR_01.Scripts/Generation/DungeonGraph.cs
@@ -25,4 +25,20 @@ public class DungeonGraph
         var edge = (a.x < b.x || (a.x == b.x && a.y < b.y)) ? (a, b) : (b, a);
         return edges.Contains(edge);
     }
+    public IEnumerable<Vector2Int> GetNeighbors(Vector2Int cell)
+    {
+        Vector2Int[] directions = { 
+            Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left 
+        };
+
+        foreach (var dir in directions)
+        {
+            Vector2Int neighbor = cell + dir;
+        
+            if (nodes.Contains(neighbor) && HasEdge(cell, neighbor))
+            {
+                yield return neighbor;
+            }
+        }
+    }
 }
